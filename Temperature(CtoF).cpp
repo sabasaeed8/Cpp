c0dfb29f0e0acb596b20f0b1b798fa9b2b518ac1
Saba Saeed <82726779+sabasaeed8@users.noreply.github.com>
2023-03-23 13:03:41 +0500

    Add files via upload

---
+++ Temperature(CtoF).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+
+{
+	float C,F;
+	cout<<"Temperature in Celcius=";
+	cin>>C;
+	F=((9*C)/5)+32;
+	cout<<"Temperature in Fahrenheit=";
+	cout<<F;
+}
